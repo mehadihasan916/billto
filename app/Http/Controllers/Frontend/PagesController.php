b97edf8efd174254fe29758ce788c247878216dd
mehadihasan916 <mehadi.womenindigital@gmail.com>
2025-04-27 14:33:16 +0600

    error fixed

--- app/Http/Controllers/Frontend/PagesController.php
+++ app/Http/Controllers/Frontend/PagesController.php
@@ -17,39 +17,39 @@ class PagesController extends Controller
     public function index()
     {
         // Only for google, facebook & Github check request start
-        if(Auth::check()){
+        if (Auth::check()) {
             $get_id =  Auth::user()->id;
-           $user_id_check = PaymentGetway::where('user_id',$get_id)->count();
+            $user_id_check = PaymentGetway::where('user_id', $get_id)->count();
 
-           if($user_id_check==0){
-            PaymentGetway::create([
-                'user_id'=> $get_id,
-                'amount'=>'0',
-                'subscription_package_id'=>'1',
-                'organization_package_id'=>'0',
-                'created_at'=>Carbon::now()
-            ]);
-            ComplateInvoiceCount::create([
-                'user_id'=> $get_id,
-                'invoice_count_total'=>'0',
-                'current_invoice_total'=>'0',
-                'created_at'=>Carbon::now()
-            ]);
-           }
+            if ($user_id_check == 0) {
+                PaymentGetway::create([
+                    'user_id' => $get_id,
+                    'amount' => '0',
+                    'subscription_package_id' => '1',
+                    'organization_package_id' => '0',
+                    'created_at' => Carbon::now()
+                ]);
+                ComplateInvoiceCount::create([
+                    'user_id' => $get_id,
+                    'invoice_count_total' => '0',
+                    'current_invoice_total' => '0',
+                    'created_at' => Carbon::now()
+                ]);
+            }
         }
- // Only for google, facebook & Github check request END
+        // Only for google, facebook & Github check request END
 
-     $subcription_package_free = SubscriptionPackage::Where('packageName','FREE')->orWhere('packageName','Free')->get();
-     $subcription_package_stand= SubscriptionPackage::Where('packageName','Standard')->orWhere('packageName','STANDARD')->get();
-     $subcription_package_premium = SubscriptionPackage::Where('packageName','Premium')->orWhere('packageName','PREMIUM')->get();
+        $subcription_package_free = SubscriptionPackage::Where('packageName', 'FREE')->orWhere('packageName', 'Free')->get();
+        $subcription_package_stand = SubscriptionPackage::Where('packageName', 'Standard')->orWhere('packageName', 'STANDARD')->get();
+        $subcription_package_premium = SubscriptionPackage::Where('packageName', 'Premium')->orWhere('packageName', 'PREMIUM')->get();
 
-   
 
-        return view('frontend.home',compact('subcription_package_free','subcription_package_premium','subcription_package_stand'));
+
+        return view('frontend.home', compact('subcription_package_free', 'subcription_package_premium', 'subcription_package_stand'));
     }
-    
-    
-    
+
+
+
     public function loadData(Request $request)
     {
         if ($request->ajax()) {
@@ -58,14 +58,14 @@ class PagesController extends Controller
             } else {
                 $invoice_template = InvoiceTemplate::limit(8)->get();
             }
-            
+
             if ($request->id) {
-                $invoice_template_not_com = InvoiceTemplate::where('id', '>', $request->id)->where('company','not company')->limit('4')->get();
+                $invoice_template_not_com = InvoiceTemplate::where('id', '>', $request->id)->where('company', 'not company')->limit('4')->get();
             } else {
-                $invoice_template_not_com = InvoiceTemplate::limit(8)->where('company','not company')->get();
+                $invoice_template_not_com = InvoiceTemplate::limit(8)->where('company', 'not company')->get();
             }
 
-            return view('frontend.get-load-data', compact('invoice_template','invoice_template_not_com'));
+            return view('frontend.get-load-data', compact('invoice_template', 'invoice_template_not_com'));
         }
     }
 
@@ -85,8 +85,4 @@ class PagesController extends Controller
         return $request;
         return view('frontend.create_invoice');
     }
-
-
-
-
 }
