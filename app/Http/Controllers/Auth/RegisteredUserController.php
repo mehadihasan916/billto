50e03cea5a773c338161d6b32bb200d1516d3f63
mehadihasan916 <mehadi.womenindigital@gmail.com>
2025-07-31 12:13:10 +0600

    someone registered assign free package

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -61,7 +61,7 @@ class RegisteredUserController extends Controller
         PaymentGetway::create([
             'user_id' => $get_id,
             'amount' => '0',
-            'subscription_package_id' => '1',
+            'subscription_package_id' => '1', // This is assumed to be the Free Plan
             'organization_package_id' => '0',
             'created_at' => Carbon::now()
         ]);
